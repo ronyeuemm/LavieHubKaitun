eea7fedb9fc0c0fd49104ab370f0e671cf06213b
ronyeuemm <obiiiyeuem@gmail.com>
2025-01-31 10:48:09 +0700

    Add files via upload

---
+++ LaviHub.lua
@@ -0,0 +1,39 @@
+getgenv().Key = "sxionax_8a2228bc0ab"
+getgenv().Script = "LavieHub"
+getgenv().Config = {
+    ["Team"] = "Pirates", -- Marines
+    ["FPS Boost"] = false,
+    ["Black Screen"] = false,
+    ["Lock Fragment"] = 0,
+    ["Awakening Fruit"] = true,
+    ["Hop if Other Players are Nearby"] = true,
+    ["Item"] = {
+        ["Get Pole"] = true,
+        ["Get Saber"] = true,
+        ["Skull Guitar"] = {
+            ["Enable"] = true,
+            ["Get Materials Before Go 3rd Sea"] = true
+        },
+        ["Cursed Dual Katana"] = true
+    },
+    ["Mastery"] = {
+        ["Melee"] = false, -- 600 mastery
+        ["Sword"] = {
+            ["Enable"] = true,
+            ["Method"] = "Half" -- Half/Full
+        }
+    },
+    ["Fruit"] = {
+        ["Select Fruit"] = "Magma-Magma", --Example: Dough-Dough, Leopard-Leopard,...
+        ["Snipe Fruit Selected"] = true
+    },
+    ["Hop"] = {
+        ["Hop Find Mirror"] = true,
+        ["Hop Find Valkyrie Helm"] = true,
+        ["Hop Find Fruit 1m For Auto Third Sea"] = true,
+        ["Hop For Get Tushita"] = true,
+        ["Hop Find Full Moon For Get Skull Guitar"] = true,
+        ["Hop Find Cake Queen [CDK]"] = true
+    },
+}
+loadstring(game:HttpGet("https://raw.githubusercontent.com/RISER93-ROBLOX/AfxcaxiRT1/refs/heads/main/Lurag%20Scripts"))()
